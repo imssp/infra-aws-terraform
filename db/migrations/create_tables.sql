cd72dd132255d8eb0cd9bf2119993635da4e281c
Satya Sourav Patel <satyasouravssp0@gmail.com>
2026-07-08 05:26:07 +0530

    Completed docker compose for database setup with health check and also worked on migration files for creating the tables

---
+++ db/migrations/create_tables.sql
@@ -0,0 +1,24 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS hotel_bookings (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    org_id UUID NOT NULL,
+    hotel_id VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    checkin_date DATE NOT NULL,
+    checkout_date DATE NOT NULL,
+    amount NUMERIC(12, 2) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS booking_events (
+    id BIGSERIAL PRIMARY KEY,
+    booking_id UUID NOT NULL,
+    event_type VARCHAR(100) NOT NULL,
+    payload JSONB,
+    created_at TIMESTAMP NOT NULL
+    CONSTRAINT real_booking_events
+        FOREIGN KEY (booking_id)
+        REFERENCES hotel_bookings(id) ON DELETE CASCADE
+);
